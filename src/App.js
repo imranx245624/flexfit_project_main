8c9933f82135ceb2a4cfa3f3ffbc9c6aa4539814
Imran Ali <mdimranalidbg1234567890@gmail.com>
2025-10-13 23:48:51 +0530

    First commit: Created home page UI for FlexFit project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div1" >
+      <h1>choose your workout</h1>
+      <div className='div1a'> 
+        <h3>Home workout</h3>
+        <p>train in the confort  of your home with <br/> minimal equipment</p> 
+        <button className="button1" >start </button>
+      </div>
+      <div className='div1b'> 
+        <h3>Gym workout</h3>
+        <p>Access a wide range of equipment and .<br/>maximize your training</p> 
+        <button className="button2" >start</button>
+      </div>
+
     </div>
   );
 }
